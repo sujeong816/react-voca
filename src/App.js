4bef70a49dcc7d9600df91cf6ccef794fb8dcad5
Sujeong Kim <62699938+sujeong816@users.noreply.github.com>
2023-01-07 18:02:45 +0900

    Update App.js
    Add <BrowerRouter> - line 10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
-import Hello from './component/Hello';
+import "./App.css";
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Emptypage from "./component/Emptypage";
 
 function App() {
   return (
-    <div>
-      <Hello age={10}/>
-      <Hello age={20}/>
-      <Hello age={30}/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+
+        <Routes>
+          {/** url에 따라 태그 안 내용이 달라짐 */}
+          <Route exact path="/" element={<DayList />}/>
+
+          <Route path="/day/:day" element={<Day />}/>
+
+          <Route element={<Emptypage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
