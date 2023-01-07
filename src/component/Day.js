7259adf47450534a4c7323d82acbc017abe85324
sujeong816 <sujeong1517@naver.com>
2023-01-07 17:58:31 +0900

    A: add Day.js - Word Categorization by Day

---
+++ src/component/Day.js
@@ -0,0 +1,24 @@
+import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
+import Word from "./Word";
+
+export default function Day() {
+  //   const day = useParams().day;
+  const { day } = useParams();
+  const wordList = dummy.words.filter(
+    (word) => word.day === Number(day)
+  );
+
+  return (
+    <div>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {wordList.map((word) => (
+            <Word word={word} key={word.id}/>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
