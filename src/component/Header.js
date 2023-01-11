6b4023180ada59c2d8a0436f9bc2d3e6f33e594e
Sujeong Kim <62699938+sujeong816@users.noreply.github.com>
2023-01-11 17:15:38 +0900

    Update Header.js
    <a>태그를 <Link>태그로 수정 및 클릭 시 페이지 이동하는 경로 추가

--- src/component/Header.js
+++ src/component/Header.js
@@ -7,13 +7,13 @@ export default function Header() {
             <Link to="/">토익 영단어(고급)</Link>
         </h1>
         <div className="menu">
-            <a href="#x" className="link">
+            <Link to="/create_word" className="link">
                 단어 추가
-            </a>
+            </Link>
 
-            <a href="#x" className="link">
+            <Link to="/create_day" className="link">
                 Day 추가
-            </a>
+            </Link>
         </div>
 
     </div>
