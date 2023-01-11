31efef1eaa4f4e18225f87385a41718c1d1a345e
Sujeong Kim <62699938+sujeong816@users.noreply.github.com>
2023-01-11 17:18:19 +0900

    Update Word.js
    오류로 인해 <table>, <tbody> 태그 추가, isDone 상태를 변경하여 저장하는 fetch 추가, 단어 삭제하는 함수 del 추가

--- src/component/Word.js
+++ src/component/Word.js
@@ -1,28 +1,74 @@
 import { useState } from "react";
 
-export default function Word({ word }) {
+export default function Word({ word: w }) {
+  const [word, setWord] = useState(w);
   const [isShow, setIsShow] = useState(false);
   const [isDone, setIsDone] = useState(word.isDone);
 
-  function toggleIsShow(){
-    setIsShow(!isShow)
+  function toggleIsShow() {
+    setIsShow(!isShow);
   }
 
-  function toggleIsDone(){
-    setIsDone(!isDone)
+  function toggleIsDone() {
+    // setIsDone(!isDone)
+    fetch(`http://localhost:3001/${word.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application.json",
+      },
+
+      body: JSON.stringify({
+        ...word /** 기존데이터에 */,
+        isDone: isDone /** isDone 상태를 바꿔서 저장 */,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        setIsDone(!isDone);
+      }
+    });
+  }
+
+  function del() {
+    if (window.confirm("삭제하시겠습니까?")) {
+      fetch(`http://localhost:3001/words/${word.id}`, {
+        method: "DELETE",
+      }).then((res) => {
+        if (res.ok) {
+          setWord({ id: 0 });
+        }
+      });
+    }
+  }
+
+  if (word.id === 0) {
+    return null;
   }
 
   return (
-    <tr className={isDone ? "off" : ""}>
-      <td>
-        <input type="checkbox" checked={isDone} onChange={toggleIsDone}/>
-      </td>
-      <td>{word.eng}</td>
-      <td>{isShow && word.kor}</td> {/** isShow가 true일 때만 단어 뜻이 보임 */}
-      <td>
-        <button onClick={toggleIsShow}>뜻 {isShow ? "숨기기" : "보기"}</button>
-        <button className="btn_del">삭제</button>
-      </td>
-    </tr>
+    <table>
+      <tbody>
+        <tr className={isDone ? "off" : ""}>
+          <td>
+            <input type="checkbox" checked={isDone} onChange={toggleIsDone} />
+          </td>
+          <td>{word.eng}</td>
+          <td>{isShow && word.kor}</td>{" "}
+          {/** isShow가 true일 때만 단어 뜻이 보임 */}
+          <td>
+            <button onClick={toggleIsShow}>
+              뜻 {isShow ? "숨기기" : "보기"}
+            </button>
+            <button onClick={del} className="btn_del">
+              삭제
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
+
+//Create - POST
+//Read - GET
+//Update - PUT
+//Delete - DELETE
