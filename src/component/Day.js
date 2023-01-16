a076ccf4302764b944a930e457d3623314c64560
Sujeong Kim <62699938+sujeong816@users.noreply.github.com>
2023-01-17 00:13:10 +0900

    Update Day.js
    날짜 삭제하는 함수 추가 및 완성

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "../hook/useFetch";
 import Word from "./Word";
 
@@ -8,16 +9,36 @@ export default function Day() {
 
   const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
+  const [days, setDay] = useState({day});
+
+  const navigate = useNavigate();
+
   function del() {
     /** 날짜 삭제 함수 */
     if (window.confirm("삭제하시겠습니까?")) {
+      fetch(`http://localhost:3001/days/${days.day}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application.json",
+        },
+      }).then((res) => {
+        if(res.ok){
+          setDay({id: 0});
+          navigate(`/`);
+        }
+      })
     }
   }
+
+  if(days.id === 0){
+    return null;
+  }
+
   return (
     <div>
       <div>
-        <h3>Day {day}</h3>
-        <button onClick={del} className="btn_del">
+        <h3 className="btn_h3">Day {day}</h3>
+        <button onClick={del} className="day_btn_del">
           삭제
         </button>
       </div>
